ee0290f627e7992852b5e950a272a89e14719bb9
thinkpad <donlancaster228@yandex.ru>
2021-11-30 16:40:16 +0300

    added testClass method

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,8 +1,10 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using TestsGeneratorLib;
 
 namespace Tests
@@ -35,10 +37,40 @@ namespace Tests
         [TestMethod]
         public void FilesAmountTest()
         {
-            Assert.AreEqual(4, _roots.Count, "Неверное количество созданных файлов: "+_roots.Count);
+            Assert.AreEqual(1, _roots.Count, "Неверное количество созданных файлов: "+_roots.Count);
+        }
+
+        [TestMethod]
+        public void UsingTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void MethodTest()
+        {
+
         }
 
 
+        [TestMethod]
+        public void ClassTest()
+        {
+            IEnumerable<ClassDeclarationSyntax> classes =
+              _roots[0].DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
 
+            Assert.AreEqual(1, classes.Count(), "Неверное количество классов: "+ classes.Count());
+
+            foreach (ClassDeclarationSyntax syntax in classes)
+            {
+                Assert.IsTrue(syntax.Modifiers.Any(SyntaxKind.PublicKeyword), "У класса приватный модификатор доступа");
+                Assert.AreEqual(3, syntax.Members.Count, "Неверное количество методов в классе");
+                Assert.AreEqual("TracerClassTest", syntax.Identifier.ValueText,
+                    "Название созданного класса не совпало с ожидаемым");
+                Assert.AreEqual("[TestClass]", syntax.AttributeLists.ToString(),
+                    "Атрибут созданного класса не совпал с ожиданием");
+            }
+
+        }
     }
 }
