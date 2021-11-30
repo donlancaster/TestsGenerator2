132bf26a27dc22a4e53f723f5756d3d8a0563800
thinkpad <donlancaster228@yandex.ru>
2021-11-30 14:36:45 +0300

    unit, generator classes

---
+++ TestsGeneratorLib/TestUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLib
+{
+    class TestUnit
+    {
+        public string FilePath { get; }
+        public string Source { get; }
+        public TestUnit (string filePath, string source)
+        {
+            FilePath = filePath;
+            Source = source;
+        }
+    }
+}
