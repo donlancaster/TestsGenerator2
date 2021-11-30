b4742f7837a157421a7bdf7aeea0f955cff58e2f
thinkpad <donlancaster228@yandex.ru>
2021-11-30 14:30:43 +0300

    Добавьте файлы проекта.

---
+++ TestsGeneratorLib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestsGeneratorLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
